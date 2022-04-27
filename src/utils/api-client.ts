ad8c82570dbcd740330a4cc70e9ea5533c78ee3c
Gustavo <gustavoalvesleao@gmail.com>
2022-04-27 12:55:52 -0300

    feat: add api-client util for data fetching

---
+++ src/utils/api-client.ts
@@ -0,0 +1,36 @@
+const apiURL = process.env.REACT_APP_API_URL;
+
+interface CustomConfig {
+  data?: unknown;
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  headers?: {
+    "Content-Type"?: string;
+  };
+}
+
+async function client(
+  endpoint: string,
+  { data, headers: customHeaders, ...customConfig }: CustomConfig = {}
+) {
+  const config = {
+    method: data ? "POST" : "GET",
+    body: data ? JSON.stringify(data) : undefined,
+    headers: {
+      "Content-Type": data ? "application/json" : "",
+      ...customHeaders,
+    },
+    ...customConfig,
+  };
+
+  return window
+    .fetch(`${apiURL}/${endpoint}`, config)
+    .then(async (response) => {
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      }
+      return Promise.reject(data);
+    });
+}
+
+export { client };
